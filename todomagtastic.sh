06c1410fd333c40e95a4259cc57a2d164fa08a0a
Magnús Ólafsson <magnusol93@gmail.com>
2020-01-04 18:11:41 +0100

    Add a middle folder representing a project

--- todomagtastic.sh
+++ todomagtastic.sh
@@ -2,7 +2,7 @@
 
 PROGRAM_NAME=$(basename $0)
 RC_FILE_LOCATION=~/.todomagrc
-VALID_CONFIG_VARS=("TODOMAG_PATH")
+VALID_CONFIG_VARS=("TODOMAG_PATH", "CURRENT_PROJECT")
 TODO_FOLDER_NAME="todo"
 DOING_FOLDER_NAME="doing"
 DONE_FOLDER_NAME="done"
@@ -22,35 +22,61 @@ command_help_menu(){
   echo ""
 }
 
+getProjectName(){
+  read -e projectName
+  echo "CURRENT_PROJECT=$projectName"
+  return
+}
+
 getUserPath(){
   read -e userPath
   echo "TODO_BASE_PATH=$userPath"
   return
 }
 
-command_init(){
+getCurrentProjectPath() {
   source $RC_FILE_LOCATION
+  echo "$TODO_BASE_PATH/$CURRENT_PROJECT"
+  return
+}
+
+command_init(){
+  source $RC_FILE_LOCATION 2> /dev/null
 
   if [ -f $RC_FILE_LOCATION ]; then
+
     if [ -z "$TODO_BASE_PATH" ]
     then
       echo "Where would you like to store todos?"
       todoPathVariable="$(getUserPath)"
       echo $todoPathVariable >> $RC_FILE_LOCATION
     fi
+
+    if [ -z "$CURRENT_PROJECT" ]
+    then
+      echo "Please input project name"
+      currentProjectNameVariable="$(getProjectName)"
+      echo $currentProjectNameVariable >> $RC_FILE_LOCATION
+    fi
+
   else
     echo "Config file does not exist."
     echo "Creating config file at location $RC_FILE_LOCATION"
     touch $RC_FILE_LOCATION
     echo "Where would you like to store todos?"
     todoPathVariable="$(getUserPath)"
+    echo "Please input project name"
+    currentProjectNameVariable="$(getProjectName)"
     echo $todoPathVariable >> $RC_FILE_LOCATION
+    echo $currentProjectNameVariable >> $RC_FILE_LOCATION
   fi
 
-  source $RC_FILE_LOCATION mkdir -p $TODO_BASE_PATH
-  mkdir -p "$TODO_BASE_PATH/$TODO_FOLDER_NAME"
-  mkdir -p "$TODO_BASE_PATH/$DOING_FOLDER_NAME"
-  mkdir -p "$TODO_BASE_PATH/$DONE_FOLDER_NAME"
+  source $RC_FILE_LOCATION
+  mkdir -p $TODO_BASE_PATH
+  mkdir -p "$TODO_BASE_PATH/$CURRENT_PROJECT"
+  mkdir -p "$TODO_BASE_PATH/$CURRENT_PROJECT/$TODO_FOLDER_NAME"
+  mkdir -p "$TODO_BASE_PATH/$CURRENT_PROJECT/$DOING_FOLDER_NAME"
+  mkdir -p "$TODO_BASE_PATH/$CURRENT_PROJECT/$DONE_FOLDER_NAME"
 
   echo "Setup complete!"
 }
@@ -59,9 +85,12 @@ command_add(){
   source $RC_FILE_LOCATION
   id=$(echo $RANDOM | tr '[0-9]' '[a-z]')
   task=$@
-  echo "TIMESTAMP=$(date)" >> "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$id.txt"
-  echo "ID=$id" >> "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$id.txt"
-  echo "TASK=$task" >> "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$id.txt"
+
+  projectPath="$(getCurrentProjectPath)"
+  currentTodoPath="$projectPath/$TODO_FOLDER_NAME"
+  echo "TIMESTAMP=$(date)" >> "$currentTodoPath/$id.txt"
+  echo "ID=$id" >> "$currentTodoPath/$id.txt"
+  echo "TASK=$task" >> "$currentTodoPath/$id.txt"
   echo "Task created. (id: $id)"
 }
 
@@ -73,39 +102,44 @@ command_change(){
   source $RC_FILE_LOCATION
   task_id=$@
 
-  if [ -f "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" ]; then
-    echo "Task: $(sed -n -e '/^TASK=/p' "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" | sed 's/^TASK=//') (TODO)"
+  projectPath="$(getCurrentProjectPath)"
+  currentTodoPath="$projectPath/$TODO_FOLDER_NAME"
+  currentDoingPath="$projectPath/$DOING_FOLDER_NAME"
+  currentDonePath="$projectPath/$DONE_FOLDER_NAME"
+
+  if [ -f "$currentTodoPath/$task_id.txt" ]; then
+    echo "Task: $(sed -n -e '/^TASK=/p' "$currentTodoPath/$task_id.txt" | sed 's/^TASK=//') (TODO)"
     echo "Please enter new Task Message:"
     read -e newTask
-    echo "$(sed '/^TASK=/ d' "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt")" > "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt"
-    echo "TASK=$newTask" >> "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt"
+    echo "$(sed '/^TASK=/ d' "$currentTodoPath/$task_id.txt")" > "$currentTodoPath/$task_id.txt"
+    echo "TASK=$newTask" >> "$currentTodoPath/$task_id.txt"
 
     echo "Task is changed."
-    echo "Task: $(sed -n -e '/^TASK=/p' "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" | sed 's/^TASK=//') (TODO)"
+    echo "Task: $(sed -n -e '/^TASK=/p' "$currentTodoPath/$task_id.txt" | sed 's/^TASK=//') (TODO)"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" ]; then
-    echo "Task: $(sed -n -e '/^TASK=/p' "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" | sed 's/^TASK=//') (DOING)"
+  if [ -f "$currentDoingPath/$task_id.txt" ]; then
+    echo "Task: $(sed -n -e '/^TASK=/p' "$currentDoingPath/$task_id.txt" | sed 's/^TASK=//') (DOING)"
     echo "Please enter new Task Message:"
     read -e newTask
-    echo "$(sed '/^TASK=/ d' "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt")" > "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt"
-    echo "TASK=$newTask" >> "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt"
+    echo "$(sed '/^TASK=/ d' "$currentDoingPath/$task_id.txt")" > "$currentDoingPath/$task_id.txt"
+    echo "TASK=$newTask" >> "$currentDoingPath/$task_id.txt"
 
     echo "Task is changed."
-    echo "Task: $(sed -n -e '/^TASK=/p' "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" | sed 's/^TASK=//') (DOING)"
+    echo "Task: $(sed -n -e '/^TASK=/p' "$currentDoingPath/$task_id.txt" | sed 's/^TASK=//') (DOING)"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" ]; then
-    echo "Task: $(sed -n -e '/^TASK=/p' "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" | sed 's/^TASK=//') (DONE)"
+  if [ -f "$currentDonePath/$task_id.txt" ]; then
+    echo "Task: $(sed -n -e '/^TASK=/p' "$currentDonePath/$task_id.txt" | sed 's/^TASK=//') (DONE)"
     echo "Please enter new Task Message:"
     read -e newTask
-    echo "$(sed '/^TASK=/ d' "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt")" > "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt"
-    echo "TASK=$newTask" >> "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt"
+    echo "$(sed '/^TASK=/ d' "$currentDonePath/$task_id.txt")" > "$currentDonePath/$task_id.txt"
+    echo "TASK=$newTask" >> "$currentDonePath/$task_id.txt"
 
     echo "Task is changed."
-    echo "Task: $(sed -n -e '/^TASK=/p' "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" | sed 's/^TASK=//') (DONE)"
+    echo "Task: $(sed -n -e '/^TASK=/p' "$currentDonePath/$task_id.txt" | sed 's/^TASK=//') (DONE)"
     return
   fi
 }
@@ -114,19 +148,24 @@ command_doing(){
   source $RC_FILE_LOCATION
   task_id=$@
 
-  if [ -f "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" ]; then
-    mv "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt"
+  projectPath="$(getCurrentProjectPath)"
+  currentTodoPath="$projectPath/$TODO_FOLDER_NAME"
+  currentDoingPath="$projectPath/$DOING_FOLDER_NAME"
+  currentDonePath="$projectPath/$DONE_FOLDER_NAME"
+
+  if [ -f "$currentTodoPath/$task_id.txt" ]; then
+    mv "$currentTodoPath/$task_id.txt" "$currentDoingPath/$task_id.txt"
     echo "TaskId: $task_id was moved from TODO to DOING"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" ]; then
-    mv "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt"
+  if [ -f "$currentDonePath/$task_id.txt" ]; then
+    mv "$currentDonePath/$task_id.txt" "$currentDoingPath/$task_id.txt"
     echo "TaskId: $task_id was moved from DONE to DOING"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" ]; then
+  if [ -f "$currentDoingPath/$task_id.txt" ]; then
     echo "TaskId: $task_id is already in DOING"
     return
   fi
@@ -138,19 +177,24 @@ command_done(){
   source $RC_FILE_LOCATION
   task_id=$@
 
-  if [ -f "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" ]; then
-    mv "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt"
+  projectPath="$(getCurrentProjectPath)"
+  currentTodoPath="$projectPath/$TODO_FOLDER_NAME"
+  currentDoingPath="$projectPath/$DOING_FOLDER_NAME"
+  currentDonePath="$projectPath/$DONE_FOLDER_NAME"
+
+  if [ -f "$currentTodoPath/$task_id.txt" ]; then
+    mv "$currentTodoPath/$task_id.txt" "$currentDonePath/$task_id.txt"
     echo "TaskId: $task_id was moved from TODO to DONE"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" ]; then
-    mv "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt"
+  if [ -f "$currentDoingPath/$task_id.txt" ]; then
+    mv "$currentDoingPath/$task_id.txt" "$currentDonePath/$task_id.txt"
     echo "TaskId: $task_id was moved from DOING to DONE"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" ]; then
+  if [ -f "$currentDonePath/$task_id.txt" ]; then
     echo "TaskId: $task_id is already in DONE"
     return
   fi
@@ -162,19 +206,24 @@ command_todo(){
   source $RC_FILE_LOCATION
   task_id=$@
 
-  if [ -f "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" ]; then
-    mv "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt"
+  projectPath="$(getCurrentProjectPath)"
+  currentTodoPath="$projectPath/$TODO_FOLDER_NAME"
+  currentDoingPath="$projectPath/$DOING_FOLDER_NAME"
+  currentDonePath="$projectPath/$DONE_FOLDER_NAME"
+
+  if [ -f "$currentDoingPath/$task_id.txt" ]; then
+    mv "$currentDoingPath/$task_id.txt" "$currentTodoPath/$task_id.txt"
     echo "TaskId: $task_id was moved from DOING to TODO"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" ]; then
-    mv "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt"
+  if [ -f "$currentDonePath/$task_id.txt" ]; then
+    mv "$currentDonePath/$task_id.txt" "$currentTodoPath/$task_id.txt"
     echo "TaskId: $task_id was moved from DONE to TODO"
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" ]; then
+  if [ -f "$currentTodoPath/$task_id.txt" ]; then
     echo "TaskId: $task_id is already in TODO"
     return
   fi
@@ -186,20 +235,25 @@ command_delete(){
   source $RC_FILE_LOCATION
   task_id=$@
 
-  if [ -f "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt" ]; then
-    rm  -f "$TODO_BASE_PATH/$TODO_FOLDER_NAME/$task_id.txt"
+  projectPath="$(getCurrentProjectPath)"
+  currentTodoPath="$projectPath/$TODO_FOLDER_NAME"
+  currentDoingPath="$projectPath/$DOING_FOLDER_NAME"
+  currentDonePath="$projectPath/$DONE_FOLDER_NAME"
+
+  if [ -f "$currentTodoPath/$task_id.txt" ]; then
+    rm  -f "$currentTodoPath/$task_id.txt"
     echo "Task deleted."
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt" ]; then
-    rm  -f "$TODO_BASE_PATH/$DOING_FOLDER_NAME/$task_id.txt"
+  if [ -f "$currentDoingPath/$task_id.txt" ]; then
+    rm  -f "$currentDoingPath/$task_id.txt"
     echo "Task deleted."
     return
   fi
 
-  if [ -f "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt" ]; then
-    rm  -f "$TODO_BASE_PATH/$DONE_FOLDER_NAME/$task_id.txt"
+  if [ -f "$currentDonePath/$task_id.txt" ]; then
+    rm  -f "$currentDonePath/$task_id.txt"
     echo "Task deleted."
     return
   fi
@@ -210,16 +264,21 @@ command_delete(){
 command_ls(){
   source $RC_FILE_LOCATION
 
-  num_of_todos=$(ls -1 "$TODO_BASE_PATH/$TODO_FOLDER_NAME" | wc -l | tr -d '[:space:]')
-  num_of_doings=$(ls -1 "$TODO_BASE_PATH/$DOING_FOLDER_NAME" | wc -l | tr -d '[:space:]')
-  num_of_dones=$(ls -1 "$TODO_BASE_PATH/$DONE_FOLDER_NAME" | wc -l | tr -d '[:space:]')
+  projectPath="$(getCurrentProjectPath)"
+  currentTodoPath="$projectPath/$TODO_FOLDER_NAME"
+  currentDoingPath="$projectPath/$DOING_FOLDER_NAME"
+  currentDonePath="$projectPath/$DONE_FOLDER_NAME"
+
+  num_of_todos=$(ls -1 "$currentTodoPath" | wc -l | tr -d '[:space:]')
+  num_of_doings=$(ls -1 "$currentDoingPath" | wc -l | tr -d '[:space:]')
+  num_of_dones=$(ls -1 "$currentDonePath" | wc -l | tr -d '[:space:]')
 
   echo "TODO ($num_of_todos):"
   shopt -s nullglob
 
   if [ "$num_of_todos" -gt "0" ]; then
     declare -a todoArray
-    for file in "$TODO_BASE_PATH/$TODO_FOLDER_NAME/*.txt"
+    for file in "$currentTodoPath/*.txt"
     do
       todoArray=(${todoArray[*]} $file)
     done
@@ -235,7 +294,7 @@ command_ls(){
 
   if [ "$num_of_doings" -gt "0" ]; then
     declare -a doingArray
-    for file in "$TODO_BASE_PATH/$DOING_FOLDER_NAME/*.txt"
+    for file in "$currentDoingPath/*.txt"
     do
       doingArray=(${doingArray[*]} $file)
     done
@@ -251,7 +310,7 @@ command_ls(){
 
   if [ "$num_of_dones" -gt "0" ]; then
     declare -a doneArray
-    for file in "$TODO_BASE_PATH/$DONE_FOLDER_NAME/*.txt"
+    for file in "$currentDonePath/*.txt"
     do
       doneArray=(${doneArray[*]} $file)
     done
